6b945da57e6dfe1b2afe5496a772165d2d46d9fa
thomas-armena <thomasarmena@gmail.com>
2020-09-23 15:44:33 -0400

    Initial Commit

---
+++ ElmJrMetalEdition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElmJrMetalEdition
+//
+//  Created by Thomas Armena on 2020-09-23.
+//  Copyright © 2020 Thomas Armena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
