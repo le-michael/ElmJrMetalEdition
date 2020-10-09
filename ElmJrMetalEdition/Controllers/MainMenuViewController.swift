64a5557a8b9b57e578db16f96e0b5a6e1a36cdbe
thomas-armena <thomasarmena@gmail.com>
2020-10-09 17:17:16 -0400

    Added metal workspaces

--- ElmJrMetalEdition/Controllers/MainMenuViewController.swift
+++ ElmJrMetalEdition/Controllers/MainMenuViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class MainMenuViewController: UIViewController {
     
     let nextButton = UIButton()
+    let metalWorkspaceButton = UIButton()
+    let metalWorkspace2Button = UIButton()
+    
+    let buttonStack = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,26 +23,56 @@ class MainMenuViewController: UIViewController {
     
     func setupViews() {
         view.backgroundColor = .orange
-        setupNextButton()
+        setupButtons()
     }
     
-    func setupNextButton(){
-        view.addSubview(nextButton)
+    func setupButtons(){
+        view.addSubview(buttonStack)
+        buttonStack.alignment = .bottom;
+        buttonStack.axis = .vertical;
+        buttonStack.distribution = .fillEqually;
+        buttonStack.spacing = 16;
+
+        buttonStack.translatesAutoresizingMaskIntoConstraints = false;
+        buttonStack.heightAnchor.constraint(equalToConstant: 300).isActive = true;
+        buttonStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16).isActive = true;
+        buttonStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true;
+        buttonStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true;
+        
+        buttonStack.addArrangedSubview(nextButton);
+        buttonStack.addArrangedSubview(metalWorkspaceButton);
+        buttonStack.addArrangedSubview(metalWorkspace2Button);
+        
         nextButton.backgroundColor = .blue
         nextButton.setTitle("Go To Editor", for: .normal)
         nextButton.addTarget(self, action: #selector(nextButtonClicked(_ :)), for: .touchUpInside)
         
-        nextButton.translatesAutoresizingMaskIntoConstraints = false;
-        nextButton.heightAnchor.constraint(equalToConstant: 64).isActive = true;
-        nextButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16).isActive = true;
-        nextButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true;
-        nextButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true;
+        metalWorkspaceButton.backgroundColor = .blue
+        metalWorkspaceButton.setTitle("Go To Metal Workspace 1", for: .normal)
+        metalWorkspaceButton.addTarget(self, action: #selector(metalWorkspaceButtonClicked(_ :)), for: .touchUpInside)
+        
+        metalWorkspace2Button.backgroundColor = .blue
+        metalWorkspace2Button.setTitle("Go To Metal Workspace 2", for: .normal)
+        metalWorkspace2Button.addTarget(self, action: #selector(metalWorkspace2ButtonClicked(_ :)), for: .touchUpInside)
+        
     }
     
     @objc func nextButtonClicked(_: UIButton!){
         let editorViewController = EditorViewController()
         editorViewController.modalPresentationStyle = .fullScreen
         self.present(editorViewController, animated: true, completion: nil)
+    }
+    
+    @objc func metalWorkspaceButtonClicked(_: UIButton!){
+        let metalWorkspaceController = MetalWorkspaceController()
+        metalWorkspaceController.modalPresentationStyle = .fullScreen
+        self.present(metalWorkspaceController, animated: true, completion: nil)
+    }
+    
+    @objc func metalWorkspace2ButtonClicked(_: UIButton!){
+        let metalWorkspace2Controller = MetalWorkspace2Controller()
+        metalWorkspace2Controller.modalPresentationStyle = .fullScreen
+        self.present(metalWorkspace2Controller, animated: true, completion: nil)
     }
 
 }
---
+++ ElmJrMetalEdition/Controllers/MetalWorkspaceController.swift
@@ -0,0 +1,36 @@
+//
+//  MetalWorkspaceController.swift
+//  ElmJrMetalEdition
+//
+//  Created by Thomas Armena on 2020-10-09.
+//  Copyright © 2020 Thomas Armena. All rights reserved.
+//
+
+import UIKit
+import MetalKit
+
+class MetalWorkspaceController : UIViewController {
+    let mtkView = MTKView()
+    
+    var device: MTLDevice!
+    var renderer: MTKViewDelegate!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        view.addSubview(mtkView)
+        mtkView.translatesAutoresizingMaskIntoConstraints = false
+        mtkView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        mtkView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        mtkView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        mtkView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1).isActive = true
+        
+        mtkView.device = MTLCreateSystemDefaultDevice()
+        device = mtkView.device
+        
+        mtkView.clearColor = MTLClearColorMake(0.1, 0.2, 0.8, 1.0)
+        
+        renderer = Renderer(device: device)
+        mtkView.delegate = renderer
+    }
+}
