fe6b782e858b347eb1374c0c86f2540b5570e5e6
le-michael <lem12@mcmaster.ca>
2020-11-16 23:09:37 -0500

    Remove unused import and reformat code

--- ElmJrMetalEdition/Controllers/MetalWorkspaceController.swift
+++ ElmJrMetalEdition/Controllers/MetalWorkspaceController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import MetalKit
-import GLKit
 
 class MetalWorkspaceController : UIViewController {
     let mtkView = MTKView()
@@ -30,9 +29,21 @@ class MetalWorkspaceController : UIViewController {
         device = mtkView.device
         
         let scene = Scene(device: device)
-        let triangle1 = Triangle(xPos: 0.5, yPos: 0.5, size: 0.5, color: simd_float4(0.0, 0.0, 1.0, 1.0), device: device)
+        let triangle1 = Triangle(
+            xPos: 0.5,
+            yPos: 0.5,
+            size: 0.5,
+            color: simd_float4(0.0, 0.0, 1.0, 1.0),
+            device: device
+        )
         scene.addChild(node: triangle1)
-        let triangle2 = Triangle(xPos: -0.4, yPos: -0.4, size: 0.25, color: simd_float4(0.0, 1.0, 0.0, 1.0), device: device)
+        let triangle2 = Triangle(
+            xPos: -0.4,
+            yPos: -0.4,
+            size: 0.25,
+            color: simd_float4(0.0, 1.0, 0.0, 1.0),
+            device: device
+        )
         scene.addChild(node: triangle2)
         
         mtkView.clearColor = MTLClearColorMake(0.0, 0.0, 0.0, 1.0)
