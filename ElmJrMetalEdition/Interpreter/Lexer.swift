03a708aaba37eadfa3e8802b8c975df08b6f31ff
le-michael <lem12@mcmaster.ca>
2020-11-17 21:54:20 -0500

    Create Interpreter directory

---
+++ ElmJrMetalEdition/Interpreter/Lexer.swift
@@ -0,0 +1,9 @@
+//
+//  Lexer.swift
+//  ElmJrMetalEdition
+//
+//  Created by Michael Le on 2020-11-17.
+//  Copyright © 2020 Thomas Armena. All rights reserved.
+//
+
+import Foundation
---
+++ ElmJrMetalEdition/Interpreter/Parser.swift
@@ -0,0 +1,9 @@
+//
+//  Parser.swift
+//  ElmJrMetalEdition
+//
+//  Created by Michael Le on 2020-11-17.
+//  Copyright © 2020 Thomas Armena. All rights reserved.
+//
+
+import Foundation
