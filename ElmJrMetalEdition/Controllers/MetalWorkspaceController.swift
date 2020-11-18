37637ab5b20c31843d2d99c10294c0047b67ab26
le-michael <lem12@mcmaster.ca>
2020-11-17 19:14:27 -0500

    Remake API for Triangle class, added transformation matricies

--- ElmJrMetalEdition/Controllers/MetalWorkspaceController.swift
+++ ElmJrMetalEdition/Controllers/MetalWorkspaceController.swift
@@ -6,15 +6,15 @@
 //  Copyright © 2020 Thomas Armena. All rights reserved.
 //
 
-import UIKit
 import MetalKit
+import UIKit
 
-class MetalWorkspaceController : UIViewController {
+class MetalWorkspaceController: UIViewController {
     let mtkView = MTKView()
-    
+
     var device: MTLDevice!
     var renderer: MTKViewDelegate!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
@@ -24,28 +24,35 @@ class MetalWorkspaceController : UIViewController {
         mtkView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
         mtkView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
         mtkView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1).isActive = true
-        
+
         mtkView.device = MTLCreateSystemDefaultDevice()
         device = mtkView.device
-        
+
         let scene = Scene(device: device)
-        let triangle1 = Triangle(
-            xPos: 0.5,
-            yPos: 0.5,
-            size: 0.5,
-            color: simd_float4(0.0, 0.0, 1.0, 1.0),
-            device: device
-        )
+        scene.sceneProps?.viewMatrix = createTranslationMatrix(x: 0, y: 0, z: -4)
+
+        let triangle1 = Triangle(color: simd_float4(0.0, 0.0, 1.0, 1.0))
         scene.addChild(node: triangle1)
-        let triangle2 = Triangle(
-            xPos: -0.4,
-            yPos: -0.4,
-            size: 0.25,
-            color: simd_float4(0.0, 1.0, 0.0, 1.0),
-            device: device
-        )
+        
+        let triangle2 = Triangle(color: simd_float4(1.0, 0.0, 1.0, 1.0))
+        triangle2.rotationMatrix = createZRotaionMatrix(degrees: 180)
         scene.addChild(node: triangle2)
         
+        let triangle3 = Triangle(color: simd_float4(0.0, 1.0, 1.0, 1.0))
+        triangle3.translationMatrix = createTranslationMatrix(x: -0.5, y: Float(sqrt(3)/2), z: 0)
+        scene.addChild(node: triangle3)
+        
+        let triangle4 = Triangle(color: simd_float4(0.0, 1.0, 0.0, 1.0))
+        triangle4.translationMatrix = createTranslationMatrix(x: 0.5, y: -Float(sqrt(3)/2), z: 0)
+        triangle4.rotationMatrix = createZRotaionMatrix(degrees: 180)
+        scene.addChild(node: triangle4)
+        
+        let triangle5 = Triangle(color: simd_float4(1.0, 0.0, 0.0, 1.0))
+        triangle5.scaleMatrix = createScaleMatrix(x: 0.5, y: 0.5, z: 0)
+        triangle5.rotationMatrix = createZRotaionMatrix(degrees: 45)
+        triangle5.translationMatrix = createTranslationMatrix(x: 0.25, y: -0.5, z: 0)
+        scene.addChild(node: triangle5)
+
         mtkView.clearColor = MTLClearColorMake(0.0, 0.0, 0.0, 1.0)
         renderer = Renderer(device: device, view: mtkView, scene: scene)
         mtkView.delegate = renderer
--- ElmJrMetalEdition/Renderer/Nodes/Triangle.swift
+++ ElmJrMetalEdition/Renderer/Nodes/Triangle.swift
@@ -11,25 +11,27 @@ import MetalKit
 // MARK: Triangle Class
 
 class Triangle: Node {
-    let device: MTLDevice
     let indicies: [UInt16] = [0, 1, 2]
     
     var verticies: [Vertex]
     var vertexBuffer: MTLBuffer?
     var indexBuffer: MTLBuffer?
+    var modelConstants = ModelConstants()
     
-    init(xPos: Float, yPos: Float, size: Float, color: simd_float4, device: MTLDevice) {
+    var translationMatrix = matrix_identity_float4x4
+    var rotationMatrix = matrix_identity_float4x4
+    var scaleMatrix = matrix_identity_float4x4
+    
+    init(color: simd_float4) {
         verticies = [
-            Vertex(Position: simd_float3(xPos, yPos+size/2, 0), Color: color),
-            Vertex(Position: simd_float3(xPos+size, yPos-size, 0), Color: color),
-            Vertex(Position: simd_float3(xPos-size, yPos-size, 0), Color: color),
+            Vertex(Position: simd_float3(1/2, 1 * (sqrt(3)/2), 0), Color: color),
+            Vertex(Position: simd_float3(1, 0, 0), Color: color),
+            Vertex(Position: simd_float3(0, 0, 0), Color: color),
         ]
-        self.device = device
         super.init()
-        initBuffers()
     }
     
-    private func initBuffers() {
+    override func createBuffers(device: MTLDevice) {
         vertexBuffer = device.makeBuffer(
             bytes: verticies, length: verticies.count * MemoryLayout<Vertex>.stride,
             options: []
@@ -42,14 +44,27 @@ class Triangle: Node {
         )
     }
     
-    override func draw(commandEncoder: MTLRenderCommandEncoder, pipelineState: MTLRenderPipelineState) {
-        guard let indexBuffer = indexBuffer else { return }
+    private func transformationMatrix() -> matrix_float4x4 {
+        return translationMatrix * rotationMatrix * scaleMatrix
+    }
+    
+    override func draw(commandEncoder: MTLRenderCommandEncoder, pipelineState: MTLRenderPipelineState, sceneProps: SceneProps) {
+        guard let indexBuffer = indexBuffer,
+              let vertexBuffer = vertexBuffer else { return }
+        
+        let modelViewMatrix = sceneProps.viewMatrix * transformationMatrix()
+        modelConstants.modelViewMatrix = sceneProps.projectionMatrix * modelViewMatrix
         
         commandEncoder.setRenderPipelineState(pipelineState)
         commandEncoder.setVertexBuffer(
             vertexBuffer,
             offset: 0,
             index: 0
+        )
+        commandEncoder.setVertexBytes(
+            &modelConstants,
+            length: MemoryLayout<ModelConstants>.stride,
+            index: 1
         )
         commandEncoder.drawIndexedPrimitives(
             type: .triangle,
