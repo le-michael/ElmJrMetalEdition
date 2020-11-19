d744941f585e9b07279307adf80eee72efcfa82a
Michael Le <lem12@mcmaster.ca>
2020-11-18 19:38:30 -0500

    Implement N sided regular polygons (#11)
    * Implement n-agon and removed triangle class
    
    * Format code
    
    * Fix default wired value for buffer data generation function

--- ElmJrMetalEdition/Controllers/ListParser.swift
+++ ElmJrMetalEdition/Controllers/ListParser.swift
@@ -7,25 +7,23 @@
 //
 
 import Foundation
-import UIKit
 import MetalKit
+import UIKit
 
-func transpile(data: [TriangleEditNode]) -> [Triangle] {
-    
-    var triangleNodes = [Triangle]()
+func transpile(data: [TriangleEditNode]) -> [NRegularPolygon] {
+    var triangleNodes = [NRegularPolygon]()
 
     for triangle in data {
-        let tempTriangle = Triangle(color: simd_float4(triangle.color.r, triangle.color.g, triangle.color.b, triangle.color.a))
-        tempTriangle.rotationMatrix = createZRotationMatrix(degrees: triangle.rotation);
+        let tempTriangle = NRegularPolygon(numOfSides: 3, color: simd_float4(triangle.color.r, triangle.color.g, triangle.color.b, triangle.color.a))
+        tempTriangle.rotationMatrix = createZRotationMatrix(degrees: triangle.rotation)
         tempTriangle.translationMatrix = createTranslationMatrix(x: triangle.xPos, y: triangle.yPos, z: 0)
         tempTriangle.scaleMatrix = createScaleMatrix(x: triangle.size, y: triangle.size, z: 0)
         triangleNodes.append(tempTriangle)
     }
-    
+
     return triangleNodes
 }
 
-private func getColor(color: ColorEditNode) ->simd_float4 {
+private func getColor(color: ColorEditNode) -> simd_float4 {
     return simd_float4(color.r, color.b, color.g, color.a)
 }
-
---
+++ ElmJrMetalEdition/Renderer/Nodes/NRegularPolygon.swift
@@ -0,0 +1,20 @@
+//
+//  NRegularPolygon.swift
+//  ElmJrMetalEdition
+//
+//  Created by Michael Le on 2020-11-18.
+//  Copyright © 2020 Thomas Armena. All rights reserved.
+//
+
+import MetalKit
+
+class NRegularPolygon: Renderable {
+    init(numOfSides n: Int, color: simd_float4, showWireFrame: Bool = false) {
+        let bufferData = nRegularPolygonBufferData(numOfSides: n, wired: showWireFrame)
+        let vertices = bufferData.vertexPositions.map {
+            Vertex(Position: $0, Color: color)
+        }
+        
+        super.init(mesh: Mesh(vertices: vertices, indices: bufferData.indices, showWireFrame: showWireFrame))
+    }
+}
