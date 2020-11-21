8fa79a311fb5be6e6ca3c643d9fabfe3731e56a7
le-michael <lem12@mcmaster.ca>
2020-11-20 21:44:42 -0500

    Add line renderable

--- ElmJrMetalEdition/Renderer/RendererTypes.swift
+++ ElmJrMetalEdition/Renderer/RendererTypes.swift
@@ -9,9 +9,23 @@
 import MetalKit
 import simd
 
-struct BufferData {
+class BufferData {
     var vertexPositions: [simd_float3]
     var indices: [UInt16]
+
+    init(vertexPositions: [simd_float3], indices: [UInt16]) {
+        self.vertexPositions = vertexPositions
+        self.indices = indices
+    }
+
+    func applyTransform(transformMatrix: matrix_float4x4) {
+        let newVertexPositions: [simd_float3] = self.vertexPositions.map {
+            var tempPos = simd_float4($0.x, $0.y, $0.z, 1)
+            tempPos = transformMatrix * tempPos
+            return simd_float3(tempPos.x, tempPos.y, tempPos.z)
+        }
+        self.vertexPositions = newVertexPositions
+    }
 }
 
 struct Vertex {
