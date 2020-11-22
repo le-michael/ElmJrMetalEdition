0bc4772ad499e137caec74f60925b28f2157e6ea
Michael Le <lem12@mcmaster.ca>
2020-11-22 18:05:24 -0500

    Add line size argument for Line2D (#25)
    * Add line size argument
    
    * Format code
    
    * Remove test comments

--- ElmJrMetalEdition/Renderer/GraphicsNodes/Renderable/Line2D.swift
+++ ElmJrMetalEdition/Renderer/GraphicsNodes/Renderable/Line2D.swift
@@ -10,17 +10,17 @@ import GLKit
 import simd
 
 class Line2D: Renderable {
-    init(p0: simd_float3, p1: simd_float3, color: simd_float4) {
+    init(p0: simd_float3, p1: simd_float3, size: Float, color: simd_float4) {
         let bufferData = planeBufferData()
         
         let magnitude = simd_distance(p0, p1)
-        bufferData.applyTransform(transformMatrix: createScaleMatrix(x: 0.01, y: magnitude, z: 1))
+        bufferData.applyTransform(transformMatrix: createScaleMatrix(x: size, y: magnitude, z: 1))
         
         let v = p1 - p0
-        let angle = -atan(v.x/v.y) //atan2(simd_cross(p0, p1).z, simd_dot(p0, p1))//acos(dotProduct / (simd_length(p0) * simd_length(p1)))
+        let angle = -atan(v.x/v.y)
         bufferData.applyTransform(transformMatrix: createZRotationMatrix(radians: angle))
         
-        let midPoint = p0+v/2
+        let midPoint = p0 + v/2
         bufferData.applyTransform(transformMatrix: createTranslationMatrix(x: midPoint.x, y: midPoint.y, z: midPoint.z))
         
         let vertices = bufferData.vertexPositions.map {
