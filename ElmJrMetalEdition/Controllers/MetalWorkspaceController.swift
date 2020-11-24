fa88c7218b9c3a1593bcf9cd8f799d7b4484bc71
Michael Le <lem12@mcmaster.ca>
2020-11-24 17:01:04 -0500

    Add metal demo and change perspective to be 100 units instead of 4 units (#31)

--- ElmJrMetalEdition/Controllers/MetalWorkspaceController.swift
+++ ElmJrMetalEdition/Controllers/MetalWorkspaceController.swift
@@ -29,58 +29,106 @@ class MetalWorkspaceController: UIViewController {
         device = mtkView.device
 
         let scene = Scene(device: device)
-        scene.sceneProps?.viewMatrix = createTranslationMatrix(x: 0, y: 0, z: -4)
+        scene.sceneProps?.viewMatrix = createTranslationMatrix(x: 0, y: 0, z: -100)
 
-        // Triangle
-
-        let poly1 = RegularPolygon(35)
-        poly1.triangleFillMode = .lines
-        poly1.transform.zRotationMatrix.angleEquation = RMUnaryOp(type: .cos, child: RMTime())
-        scene.add(poly1)
-
-        let poly2 = RegularPolygon(50)
-        poly2.triangleFillMode = .lines
-        poly2.transform.zRotationMatrix.angleEquation = RMUnaryOp(type: .sin, child: RMTime())
-        scene.add(poly2)
-
-        let plane1 = Plane()
-        plane1.triangleFillMode = .lines
-        scene.add(plane1)
-
-        let line1 = Line2D(p0: simd_float3(-0.5, 0.5, 0), p1: simd_float3(0.5, -0.5, 0), size: 0.01)
+        let line1 = Line2D(p0: simd_float3(-200, 0 , 0), p1: simd_float3(200, 0, 0), size: 0.5)
+        line1.transform.zRotationMatrix.setZRotation(angle: RMTime())
         scene.add(line1)
-
-        let point1 = RegularPolygon(4)
-
-        point1.transform.translationMatrix.xEquation = RMConstant(2)
-        point1.transform.translationMatrix.yEquation = RMConstant(2)
-        point1.transform.scaleMatrix.xEquation = RMConstant(0.03)
-        point1.transform.scaleMatrix.yEquation = RMConstant(0.03)
-        scene.add(point1)
-
-        let point2 = RegularPolygon(4)
-        point2.transform.scaleMatrix.xEquation = RMConstant(0.03)
-        point2.transform.scaleMatrix.yEquation = RMConstant(0.03)
-        point2.transform.translationMatrix.setTranslation(
-            x: RMConstant(1.5),
-            y: RMUnaryOp(type: .sin, child: RMTime()),
-            z: RMConstant(1)
-        )
-        scene.add(point2)
-
-        let line2 = Line2D(p0: simd_float3(2, 2, 0), p1: simd_float3(1.5, 1.5, 0), size: 0.03)
+        
+        let line2 = Line2D(p0: simd_float3(0, -200 , 0), p1: simd_float3(0, 200, 0), size: 0.5)
+        line2.transform.zRotationMatrix.setZRotation(angle: RMTime())
         scene.add(line2)
+        
+        let planet1 = RegularPolygon(5)
+        planet1.transform.scaleMatrix.setScale(x: 3, y: 3, z: 1)
+        planet1.transform.translationMatrix.setTranslation(
+            x: RMBinaryOp(
+                type: .mul,
+                leftChild: RMConstant(30),
+                rightChild: RMUnaryOp(type: .sin, child: RMTime())
+            ),
+            y: RMBinaryOp(
+                type: .mul,
+                leftChild: RMConstant(30),
+                rightChild: RMUnaryOp(type: .cos, child: RMTime())
+            ),
+            z: RMConstant(0)
+        )
+        planet1.transform.zRotationMatrix.setZRotation(angle: RMTime())
+        planet1.color.setColor(
+            r: RMConstant(1),
+            g: RMUnaryOp(type: .sin, child: RMTime()),
+            b: RMUnaryOp(type: .cos, child: RMTime()),
+            a: RMConstant(1)
+        )
+        scene.add(planet1)
 
-        let poly3 = RegularPolygon(50)
-        poly3.color.setColor(
+        let planet2 = RegularPolygon(4)
+        planet2.transform.scaleMatrix.setScale(x: 3, y: 3, z: 1)
+        planet2.transform.translationMatrix.setTranslation(
+            x: RMBinaryOp(
+                type: .mul,
+                leftChild: RMConstant(20),
+                rightChild: RMUnaryOp(
+                    type: .sin,
+                    child: RMBinaryOp(
+                        type: .div,
+                        leftChild: RMTime(),
+                        rightChild: RMConstant(2)
+                    )
+                )
+            ),
+            y: RMBinaryOp(
+                type: .mul,
+                leftChild: RMConstant(20),
+                rightChild: RMUnaryOp(
+                    type: .cos,
+                    child: RMBinaryOp(
+                        type: .div,
+                        leftChild: RMTime(),
+                        rightChild: RMConstant(2)
+                    )
+                )
+            ),
+            z: RMConstant(0)
+        )
+        planet2.color.setColor(
             r: RMUnaryOp(type: .cos, child: RMTime()),
             g: RMUnaryOp(type: .sin, child: RMTime()),
             b: RMConstant(1),
             a: RMConstant(1)
         )
-        poly3.transform.translationMatrix.setTranslation(x: -1.5, y: -1, z: 0)
-        poly3.transform.scaleMatrix.setScale(x: 0.75, y: 0.75, z: 1)
-        scene.add(poly3)
+        planet2.transform.zRotationMatrix.setZRotation(angle: RMTime())
+        scene.add(planet2)
+
+        let planet3 = RegularPolygon(3)
+        planet3.transform.scaleMatrix.setScale(x: 3, y: 3, z: 1)
+        planet3.transform.translationMatrix.setTranslation(
+            x: RMBinaryOp(
+                type: .mul,
+                leftChild: RMConstant(10),
+                rightChild: RMUnaryOp(type: .sin, child: RMTime())
+            ),
+            y: RMBinaryOp(
+                type: .mul,
+                leftChild: RMConstant(10),
+                rightChild: RMUnaryOp(type: .cos, child: RMTime())
+            ),
+            z: RMConstant(0)
+        )
+        planet3.color.setColor(
+            r: RMUnaryOp(type: .sin, child: RMTime()),
+            g: RMConstant(1),
+            b: RMUnaryOp(type: .cos, child: RMTime()),
+            a: RMConstant(1)
+        )
+        planet3.transform.zRotationMatrix.setZRotation(angle: RMTime())
+        scene.add(planet3)
+
+        let sun = RegularPolygon(30)
+        sun.transform.scaleMatrix.setScale(x: 5, y: 5, z: 1)
+        sun.color.setColor(r: 1, g: 1, b: 0, a: 0)
+        scene.add(sun)
 
         mtkView.clearColor = MTLClearColorMake(0.0, 0.0, 0.0, 1.0)
         renderer = Renderer(device: device, view: mtkView, scene: scene)
