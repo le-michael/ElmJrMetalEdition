a4173a3121b5a53b262c68a8944c3d4bac965cbc
le-michael <lem12@mcmaster.ca>
2020-11-23 22:47:48 -0500

    Add binary operations and a couple unary operations

---
+++ ElmJrMetalEdition/Renderer/MathNodes/RMBinaryOp.swift
@@ -0,0 +1,41 @@
+//
+//  RMBinaryOp.swift
+//  ElmJrMetalEdition
+//
+//  Created by Michael Le on 2020-11-23.
+//  Copyright © 2020 Thomas Armena. All rights reserved.
+//
+
+import Foundation
+
+enum RMBinaryOpType {
+    case add
+    case sub
+    case mul
+    case div
+}
+
+class RMBinaryOp: RMNode {
+    var type: RMBinaryOpType
+    var leftChild: RMNode
+    var rightChild: RMNode
+
+    init(type: RMBinaryOpType, leftChild: RMNode, rightChild: RMNode) {
+        self.type = type
+        self.leftChild = leftChild
+        self.rightChild = rightChild
+    }
+
+    func evaluate(_ sceneProps: SceneProps) -> Float {
+        switch self.type {
+        case .add:
+            return leftChild.evaluate(sceneProps) + rightChild.evaluate(sceneProps)
+        case .sub:
+            return leftChild.evaluate(sceneProps) - rightChild.evaluate(sceneProps)
+        case .mul:
+            return leftChild.evaluate(sceneProps) * rightChild.evaluate(sceneProps)
+        case .div:
+            return leftChild.evaluate(sceneProps) / rightChild.evaluate(sceneProps)
+        }
+    }
+}
