df250e9f6a087a33d819afad48c5f85f3bcaa4b3
Necried <duttonl@mcmaster.ca>
2020-12-08 21:10:48 -0500

    Finish unification implementation

--- ElmJrMetalEdition/Interpreter/EITypeInferencer.swift
+++ ElmJrMetalEdition/Interpreter/EITypeInferencer.swift
@@ -8,46 +8,67 @@
 
 import Foundation
 
+protocol Substitutable {}
+
 class EITypeInferencer {
     
     init () {}
     
     // A type environment from variables to schemes
     typealias TypeEnv = [Var : Scheme]
-    typealias Var = String
     
+    // Alias variables and type variables to strings
+    typealias Var = String
+    typealias TVar = String
+    
+    // global variables: The type environment, and a fresh variable counter
     var typeEnv = TypeEnv()
+    var counter = -1
+    
+    // Type checking errors
+    enum TypeError : Error {
+        case UnificationFail(MonoType, MonoType)
+        case InfiniteType(TVar, MonoType)
+        case UnboundedVariable(String)
+    }
     
     // monomorphic types
-    enum MonoType {
-        case TVar(String)
+    enum MonoType : Equatable {
+        case TVar(TVar)
         case TCon(String)
         indirect case TArr(MonoType, MonoType)
     }
     
     // polymorphic type schemes
     class Scheme {
-        var tyVars : [String]
+        var tyVars : [TVar]
         var ty : MonoType
         
-        init(tyVars : [String], ty : MonoType) {
+        init(tyVars : [TVar], ty : MonoType) {
             self.tyVars = tyVars
             self.ty = ty
         }
     }
     
-    func extend(_ varname : Var, _ scheme : Scheme) {
-        typeEnv[varname] = scheme
+    func extend(_ typeEnv : TypeEnv, _ varname : Var, _ scheme : Scheme) -> TypeEnv {
+        var newTypeEnv = typeEnv
+        newTypeEnv[varname] = scheme
+        return newTypeEnv
     }
     
     // Substitution of type variables to types
-    typealias Subst = [String : MonoType]
+    typealias Subst = [TVar : MonoType]
     
     let nullSubst = Subst()
     
-    // unimplemented
+    // Apply substitution `s1` then `s2`
     func compose(_ s1 : Subst, _ s2 : Subst) -> Subst {
-        return nullSubst
+        var newSubst = s1
+        let s2App = s2.mapValues( { (tyVar : MonoType) -> MonoType in return apply(s1, with : tyVar) })
+        for (k, v) in s2App {
+            newSubst[k] = v
+        }
+        return newSubst
     }
     
     // Apply a substitution to a monotype
@@ -82,9 +103,72 @@ class EITypeInferencer {
         return Scheme(tyVars : scheme.tyVars, ty : tyRec)
     }
  
-    func apply(_ s : inout Subst) {
-        for (tyvar, scheme) in typeEnv {
-            typeEnv[tyvar] = apply(s, with : scheme)
+    func apply(_ s : Subst, with tEnv : TypeEnv) -> TypeEnv {
+        var newTypeEnv = tEnv
+        for (tyvar, scheme) in newTypeEnv {
+            newTypeEnv[tyvar] = apply(s, with : scheme)
+        }
+        return newTypeEnv
+    }
+    
+    func ftv(with tyVar : MonoType) -> Set<TVar> {
+        switch tyVar {
+        case .TCon( _):
+            return []
+        case .TVar(let a):
+            return [a]
+        case .TArr(let t1, let t2):
+            return ftv(with: t1).union(ftv(with: t2))
         }
     }
+    
+    func ftv(with scheme : Scheme) -> Set<TVar> {
+        return ftv(with : scheme.ty).subtracting(scheme.tyVars)
+    }
+    
+    func ftv(with tEnv : TypeEnv) -> Set<TVar> {
+        var ftvSet : Set<TVar> = []
+        for sch in tEnv.values {
+            ftvSet = ftvSet.union(ftv(with : sch))
+        }
+        return ftvSet
+    }
+    
+    func fresh() -> String {
+        counter += 1
+        return "v" + String(counter)
+    }
+    
+    // Unification of two expressions under a substituion
+    func unify(_ t1 : MonoType, _ t2 : MonoType) throws -> Subst {
+        switch(t1, t2) {
+        case (.TArr(let l1, let r1), .TArr(let l2, let r2)):
+            let s1 = try unify(l1, l2)
+            let s2 = try unify(apply(s1, with : r1), apply(s1, with : r2))
+            return compose(s2, s1)
+        case(.TVar(let a), let t):
+            return try bind(a, t)
+        case(let t, .TVar(let a)):
+            return try bind(a, t)
+        case(.TCon(let a), .TCon(let b)) where a == b:
+            return nullSubst
+        default:
+            throw TypeError.UnificationFail(t1, t2)
+        }
+    }
+        
+    func bind(_ a : TVar, _ t : MonoType) throws -> Subst {
+        if (t == MonoType.TVar(a)) {
+            return nullSubst
+        } else if (occursCheck(a, t)) {
+            throw TypeError.InfiniteType(a, t)
+        } else {
+            return [a : t]
+        }
+    }
+    
+    func occursCheck(_ a : String, _ t : MonoType) -> Bool {
+        return ftv(with : t).contains(a)
+    }
+    
 }
