12590f71b9f233565ab7bcac222437428dcdd27d
thomas-armena <thomasarmena@gmail.com>
2021-01-02 03:51:29 -0500

    Deleted old code and moved some view files around

--- ElmJrMetalEdition/View/MainMenu/EVMainMenuViewController.swift
+++ ElmJrMetalEdition/View/MainMenu/EVMainMenuViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainMenuViewController: UIViewController {
+class EVMainMenuViewController: UIViewController {
     
     let nextButton = UIButton()
     let metalWorkspaceButton = UIButton()
