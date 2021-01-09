d14d3f2da110bbfe93e9d7a0e7d952cb52260e30
Necried <duttonl@mcmaster.ca>
2021-01-08 19:51:35 -0500

    Add IfElse unit test

--- UnitTests/Interpreter/EITypeInferencerTests.swift
+++ UnitTests/Interpreter/EITypeInferencerTests.swift
@@ -11,24 +11,28 @@ import XCTest
 @testable import ElmJrMetalEdition
 
 class EITypeInferencerTests: XCTestCase {
-    func checkSimpleExpressionTy(_ toEvaluate : String, _ toOutput : String)
+    func checkExprTy(_ toEvaluate : String, _ toOutput : String)
         throws {
         let ast = try EIParser(text: toEvaluate).parseExpression()
-        let tyEnv = try EITypeInferencer(parsed : ["x" : ast]).inferTop()
-        XCTAssertEqual("\(try tyEnv.lookup("x").description)", toOutput)
+        let tyEnv = try EITypeInferencer(parsed : [ast]).inferTop()
+        XCTAssertEqual("\(try tyEnv.lookup("\(ast.description)").description)", toOutput)
     }
     
     func testLiteralTypes() throws {
-        try checkSimpleExpressionTy("3", "number")
-        try checkSimpleExpressionTy("3.5", "Float")
+        try checkExprTy("3", "number")
+        try checkExprTy("3.5", "Float")
     }
     
     func testOperators() throws {
-        try checkSimpleExpressionTy("3+4", "number")
-        try checkSimpleExpressionTy("3.5+4", "Float")
+        try checkExprTy("3+4", "number")
+        try checkExprTy("3.5+4", "Float")
         // variables unimplemented
         // try checkSimpleExpressionTy("3+x", "number")
         // try checkSimpleExpressionTy("x+y", "number")
         // try checkSimpleExpressionTy(("2.0+x"), "Float")
+    }
+    
+    func testIfElse() throws {
+        try checkExprTy("if True then 1 else 2", "number")
     }
 }
