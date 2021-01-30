b06ba062a12371e7979b676161df004748b992b1
Wyatt Wismer <crazy123frog@gmail.com>
2021-01-30 15:35:24 -0700

    able to interpret API3D.elm

--- UnitTests/Interpreter/EIEvaluatorTests.swift
+++ UnitTests/Interpreter/EIEvaluatorTests.swift
@@ -151,8 +151,8 @@ class EIEvaluatorTests: XCTestCase {
     }
     
     func run3DTest(_ filename: String) throws {
-        let toLoad = ["Maybe","Base"]
-        //let toLoad = ["Maybe","Base","API3D",filename]
+        //let toLoad = ["Maybe","Base","API3D"]
+        let toLoad = ["Maybe","Base","API3D",filename]
         let code = try toLoad.map{ try getElmFile($0) }.joined(separator: "\n")
         let evaluator = EIEvaluator()
         try evaluator.compile(code)
