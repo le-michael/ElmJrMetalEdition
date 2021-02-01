c0417a91ee05b6c55e9fe40bb3a642ad8ebcfd26
le-michael <lem12@mcmaster.ca>
2021-01-31 20:18:22 -0500

    Add a new renderer init that does not require a scene

--- ElmJrMetalEdition/View/Editor/EVGraphicsView.swift
+++ ElmJrMetalEdition/View/Editor/EVGraphicsView.swift
@@ -27,7 +27,7 @@ class EVGraphicsView: UIView {
 
         mtkView.device = MTLCreateSystemDefaultDevice()
 
-        renderer = EGRenderer(view: mtkView, scene: EGDemoScenes.monkeys())
+        renderer = EGRenderer(view: mtkView)
         renderer.use(scene: EGDemoScenes.snowman())
         mtkView.delegate = renderer
 
@@ -39,15 +39,21 @@ class EVGraphicsView: UIView {
     }
 
     @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        guard let scene = renderer.scene else { return }
+
         let translation = gesture.translation(in: gesture.view)
         let delta = simd_float2(Float(translation.x), Float(-translation.y))
-        renderer.scene.camera.rotate(delta: delta)
+
+        scene.camera.rotate(delta: delta)
+
         gesture.setTranslation(.zero, in: gesture.view)
     }
 
     @objc func handlePinch(gesture: UIPinchGestureRecognizer) {
+        guard let scene = renderer.scene else { return }
+
         let delta = Float(gesture.scale - previousScale)
-        renderer.scene.camera.zoom(delta: delta)
+        scene.camera.zoom(delta: delta)
 
         previousScale = gesture.scale
         if gesture.state == .ended {
