aa43e98e9033af077ff8395a12ba5c31d87ba27d
Saad Khan <thesaadfather@gmail.com>
2021-03-08 12:35:50 -0500

    Adding grouping and some optional support

--- UnitTests/Renderer/EGTranspilerTests.swift
+++ UnitTests/Renderer/EGTranspilerTests.swift
@@ -11,19 +11,7 @@ import simd
 import XCTest
 
 class GraphicsTranspilerTests: XCTestCase {
-    
-//    func testTranspile() throws {
-//        let bundle = Bundle(for: type(of: self))
-//        let path = bundle.path(forResource: "bigtest1", ofType: "elm")!
-//        let data : Data = Data(referencing: try NSData(contentsOfFile: path))
-//        let text = String(data: data, encoding: .utf8)
-//        let evaluator = EIEvaluator()
-//        try evaluator.compile(text!)
-//        transpile(node: evaluator.globals["myScene"]!)
-//        //input:
-//        //(Scene (Camera (Translate (0, 0, 0))) [(ApTransform (Translate (50, 50, 1)) (Inked (Just (RGBA 204 0 0 1)) (Polygon 3 50)))])
-//    }
-    
+
     func getElmFile(_ filename: String) throws -> String {
         //let bundle = Bundle(for: type(of: self))
         let bundle = Bundle.main
@@ -41,7 +29,8 @@ class GraphicsTranspilerTests: XCTestCase {
         print("\(evaluator.globals["scene"]!)")
         print(" ")
         print("______________________")
-        transpile(node: (evaluator.globals["scene"]!))
+        let transpiler = EGTranspiler()
+        transpiler.transpile(node: (evaluator.globals["scene"]!))
         print("____________")
     }
 
@@ -49,10 +38,5 @@ class GraphicsTranspilerTests: XCTestCase {
         //try run3DTest("ThreeDee")
         try run3DTest("SnowMan")
     }
-    
-    
-    //(SceneWithTime (ArcballCamera 5 (0, -1, 0) Nothing Nothing) [(DirectionalLight (RGB 0.6 0.6 0.6) (1, 2, 2) (RGB 0.1 0.1 0.1)), (AmbientLight (RGB 1 1 1) 0.5)]
-    //(ApTransform (Translate (0, 2.15, 0.5)) (ApTransform (Scale (0.1, 0.1, 0.1)) (ApTransform (Rotate3D (90, 0, 0)) (Inked (Just (RGBA 0.20392157 0.68235296 0.39607844 1)) Capsule))))
-    //(ApTransform (Translate (0, 2.7, 0)) (ApTransform (Scale (0.35, 0.02, 0.35)) (Inked (Just (RGBA 0.1 0.1 0.1 1)) Cylinder)))
 }
 
