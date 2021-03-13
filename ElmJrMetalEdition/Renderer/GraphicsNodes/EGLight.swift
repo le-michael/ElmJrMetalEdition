b86d5903822159c02a97c62cf3f4545ce772fafa
le-michael <lem12@mcmaster.ca>
2021-03-13 16:19:24 -0500

    Remove commented out code

--- ElmJrMetalEdition/Renderer/GraphicsNodes/EGLight.swift
+++ ElmJrMetalEdition/Renderer/GraphicsNodes/EGLight.swift
@@ -7,48 +7,7 @@
 //
 
 import Foundation
-/*
- class EGLight {
-     static func directional(color: simd_float3, position: simd_float3, intensity: Float, specularColor: simd_float3) -> Light {
-         var light = Light()
-         light.position = position
-         light.color = color
-         light.intensity = intensity
-         light.type = Directional
-         light.specularColor = specularColor
-         return light
-     }
 
-     static func ambient(color: simd_float3, intensity: Float) -> Light {
-         var light = Light()
-         light.color = color
-         light.intensity = intensity
-         light.type = Ambient
-         return light
-     }
-
-     static func point(color: simd_float3, position: simd_float3, attenuation: simd_float3) -> Light {
-         var light = Light()
-         light.type = Point
-         light.color = color
-         light.position = position
-         light.attenuation = attenuation
-         return light
-     }
-
-     static func spotlight(color: simd_float3, position: simd_float3, attenuation: simd_float3, coneAngle: Float, coneDirection: simd_float3, coneAttenuation: Float) -> Light {
-         var light = Light()
-         light.type = Spotlight
-         light.color = color
-         light.position = position
-         light.attenuation = attenuation
-         light.coneAngle = coneAngle
-         light.coneDirection = coneDirection
-         light.coneAttenuation = coneAttenuation
-         return light
-     }
- }
- */
 typealias EGMathNode3 = (x: EGMathNode, y: EGMathNode, z: EGMathNode)
 
 class EGLight {
