86e97fc13d7f883378a512a6345814407cf9432c
Necried <duttonl@mcmaster.ca>
2021-03-14 12:55:58 -0400

    Change SnowMan example

--- ElmJrMetalEdition/Interpreter/ElmLibrary/Graphics/Examples/SnowMan.elm
+++ ElmJrMetalEdition/Interpreter/ElmLibrary/Graphics/Examples/SnowMan.elm
@@ -1,3 +1,14 @@
+hat : Shape
+hat = group
+    [ cylinder
+        |> color (rgb 0.1 0.1 0.1)
+        |> scale (0.35, 0.02, 0.35)
+        |> move (0, 2.70, 0)
+    , cylinder
+        |> color (rgb 0.1 0.1 0.1)
+        |> scale (0.25, 0.2, 0.25)
+        |> move (0, 2.90, 0)
+    ]
 
 myShapes : Float -> List Shape
 myShapes time =
@@ -24,14 +35,10 @@ myShapes time =
         |> rotate (90, 0, 0)
         |> scaleAll 0.1
         |> move (0, 2.15, 0.5)
-    , cylinder
-        |> color (rgb 0.1 0.1 0.1)
-        |> scale (0.35, 0.02, 0.35)
-        |> move (0, 2.70, 0)
-    , cylinder
-        |> color (rgb 0.1 0.1 0.1)
-        |> scale (0.25, 0.2, 0.25)
-        |> move (0, 2.90, 0)
+    , hat
+        |> scaleAll 0.75
+        |> rotate (0, 0, degToRad (-25))
+        |> move (0.2, 2.75, 0)
     , cylinder
         |> color (rgb (150/255) (70/255) 0)
         |> scale (0.05, 0.8, 0.05)
@@ -45,8 +52,8 @@ myShapes time =
     ]
 
 lights =
-    [ DirectionalLight (RGB 0.6 0.6 0.6) (1, 2, 2) (RGB 0.1 0.1 0.1)
-    , AmbientLight (RGB 1 1 1) 0.5
+    [ DirectionalLight Nothing (RGB (0.6, 0.6, 0.6)) (1, 2, 2) (RGB (0.1, 0.1, 0.1))
+    , AmbientLight Nothing (RGB (1, 1, 1)) 0.5
     ]
 
-scene = viewWithTimeAndCamera (ArcballCamera 5 (0, -1, 0) Nothing Nothing) (RGB 1 1 1) lights myShapes
+scene = viewWithTimeAndCamera (ArcballCamera 5 (0, -1, 0) Nothing Nothing) (RGB (1, 1, 1)) (\f -> lights) myShapes
